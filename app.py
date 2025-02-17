ae2a85026590e4b493e4dae753ab128144c6fc60
Piotr Druzny <druznyp@gmail.com>
2025-02-17 18:33:49 +0100

    working with Postman - binary attachment

--- app.py
+++ app.py
@@ -17,29 +17,18 @@ model = tf.keras.models.load_model("pneumonia_classification_model.keras")
 def predict():
     # Oczekujemy danych wejściowych jako JSON z kluczem "image_base64"
     print('predict - enter')
-    print('request: ', request.get_data())
-    #data = request.get_json(force=True)
-    #image_b64 = data.get("image_base64")
-    image_b64 = request.get_data(as_text=True)
 
+    print('request files: ', request.files.keys())
+    print('request data: ', request.get_data())
 
-    print(image_b64)
-    if not image_b64:
-        return jsonify({"error": "Brak 'image_base64' w danych wejściowych"}), 400
+    #if 'file' not in request.files:
+        #return jsonify({"error": "Brak pliku w żądaniu. Upewnij się, że wysyłasz plik z kluczem 'file'."}), 400
 
-    #try:
-    # Dekodowanie base64 do obrazu
-    image_bytes = base64.b64decode(image_b64)
-    print("Rozmiar danych obrazu (w bajtach):", len(image_bytes))
-    # Próba otwarcia obrazu
-    image_stream = BytesIO(image_bytes)
-    image = Image.open(image_stream)
-
-    # Weryfikacja obrazu – sprawdzenie integralności
-    image.verify()  # Uwaga: po wywołaniu verify() obraz nie może być już dalej użyty
-    # Przywrócenie strumienia do początku
-    image_stream.seek(0)
-    image = Image.open(image_stream).convert("RGB")
+    #file = request.files['file']
+    #image_b64 = request.get_data(as_text=True)
+    #image_bytes = base64.b64decode(image_b64)
+    image_bytes = request.get_data()
+    image = Image.open(BytesIO(image_bytes)).convert("RGB")
 
     image = image.resize((150, 150))
     img_array = img_to_array(image)
@@ -48,6 +37,7 @@ def predict():
     # Predykcja
     prediction = model.predict(img_array)[0][0]
     label = "Pneumonia" if prediction > 0.5 else "Normal"
+    print(jsonify({"prediction": float(prediction), "label": label}))
     return jsonify({"prediction": float(prediction), "label": label})
     '''
     except Exception as e:
