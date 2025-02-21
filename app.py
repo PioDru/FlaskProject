d35b3630e1a85ff6557a5c7b0b2dce4d78d457f8
Piotr Druzny <druznyp@gmail.com>
2025-02-21 20:02:20 +0100

    .

--- app.py
+++ app.py
@@ -223,5 +223,5 @@ def get_models():
     return jsonify({'files': list_models()})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
